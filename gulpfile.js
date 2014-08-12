c7eec5f08b6ebca314b1f628fc513e8bd09cb21b
Donna Murphy <donna.peplinskie@risevision.com>
2014-08-12 11:31:07 -0400

    Create build process
    Add gulpfile.js and package.json

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+(function () {
+  "use strict";
+
+  var gulp = require("gulp");
+  var bump = require("gulp-bump");
+  var clean = require("gulp-clean");
+  var minifyCSS = require("gulp-minify-css");
+  var rename = require("gulp-rename");
+  var runSequence = require("run-sequence");
+  var uglify = require("gulp-uglify");
+
+  var cssFiles = [
+    "src/css/bootstrap3-wysihtml5-color.css",
+    "src/css/bootstrap3-wysihtml5.css"
+  ];
+  var jsFiles = [
+    "src/js/bootstrap3-wysihtml5.js"
+  ];
+
+  gulp.task("bump", function(){
+    return gulp.src(["./package.json", "./bower.json"])
+      .pipe(bump({type:"patch"}))
+      .pipe(gulp.dest("./"));
+  });
+
+  gulp.task("clean", function () {
+    return gulp.src("dist", { read: false })
+      .pipe(clean());
+  });
+
+  gulp.task("css", function () {
+    return gulp.src(cssFiles)
+      .pipe(gulp.dest("dist/css"));
+  });
+
+  gulp.task("css-min", ["css"], function () {
+    return gulp.src("dist/css/*.css")
+      .pipe(minifyCSS({ keepBreaks: true }))
+      .pipe(rename(function(path) {
+        path.basename += ".min";
+      }))
+      .pipe(gulp.dest("dist/css"));
+  });
+
+  gulp.task("js", function () {
+    return gulp.src(jsFiles)
+      .pipe(gulp.dest("dist/js"));
+  });
+
+  gulp.task("js-uglify", ["js"], function () {
+    gulp.src("dist/js/*.js")
+      .pipe(uglify())
+      .pipe(rename(function(path) {
+        path.basename += ".min";
+      }))
+      .pipe(gulp.dest("dist/js"));
+  });
+
+  gulp.task("build", function (cb) {
+    runSequence(["clean"], ["css-min", "js-uglify"], cb);
+  });
+
+  gulp.task("default", ["build"]);
+})();
